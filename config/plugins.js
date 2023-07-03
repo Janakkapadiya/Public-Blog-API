0a5afdf58d1d0f8065dad912aa3a2d21c939f255
Janak <jaykapadiya8899@gmail.com>
2023-07-03 16:59:11 +0530

    added plugin

--- config/plugins.js
+++ config/plugins.js
@@ -2,4 +2,40 @@ module.exports = ({ env }) => ({
   seo: {
     enabled: true,
   },
+  "image-optimizer": {
+    enabled: true,
+    config: {
+      include: ["jpeg", "jpg", "png"],
+      exclude: ["gif"],
+      formats: ["original", "webp", "avif"],
+      sizes: [
+        {
+          name: "xs",
+          width: 300,
+        },
+        {
+          name: "sm",
+          width: 768,
+        },
+        {
+          name: "md",
+          width: 1280,
+        },
+        {
+          name: "lg",
+          width: 1920,
+        },
+        {
+          name: "xl",
+          width: 2840,
+          withoutEnlargement: true,
+        },
+        {
+          name: "original",
+        },
+      ],
+      additionalResolutions: [1.5, 3],
+      quality: 70,
+    },
+  },
 });
